64e1bca6337e8c12dd6ddd47353298b8bf73013d
tomo <tomo14142356@gmail.com>
2021-06-28 21:32:08 +0900

    Format code, save image

--- demo.py
+++ demo.py
@@ -1,6 +1,3 @@
-import sys
-sys.path.append('core')
-
 import argparse
 import os
 import cv2
@@ -9,13 +6,16 @@ import numpy as np
 import torch
 from PIL import Image
 
+import sys
+
+sys.path.append('core')
+
 from raft import RAFT
 from utils import flow_viz
 from utils.utils import InputPadder
 
-
-
 DEVICE = 'cuda'
+
 
 def load_image(imfile):
     img = np.array(Image.open(imfile)).astype(np.uint8)
@@ -23,10 +23,10 @@ def load_image(imfile):
     return img[None].to(DEVICE)
 
 
-def viz(img, flo):
-    img = img[0].permute(1,2,0).cpu().numpy()
-    flo = flo[0].permute(1,2,0).cpu().numpy()
-    
+def viz(img, flo, imfile):
+    img = img[0].permute(1, 2, 0).cpu().numpy()
+    flo = flo[0].permute(1, 2, 0).cpu().numpy()
+
     # map flow to rgb image
     flo = flow_viz.flow_to_image(flo)
     img_flo = np.concatenate([img, flo], axis=0)
@@ -35,8 +35,9 @@ def viz(img, flo):
     # plt.imshow(img_flo / 255.0)
     # plt.show()
 
-    cv2.imshow('image', img_flo[:, :, [2,1,0]]/255.0)
+    cv2.imshow('image', img_flo[:, :, [2, 1, 0]] / 255.0)
     cv2.waitKey()
+    cv2.imwrite(imfile, img_flo[:, :, [2, 1, 0]])
 
 
 def demo(args):
@@ -47,12 +48,16 @@ def demo(args):
     model.to(DEVICE)
     model.eval()
 
+    os.makedirs(args.output, exist_ok=True)
+
     with torch.no_grad():
         images = glob.glob(os.path.join(args.path, '*.png')) + \
                  glob.glob(os.path.join(args.path, '*.jpg'))
-        
+
         images = sorted(images)
+        i = 0
         for imfile1, imfile2 in zip(images[:-1], images[1:]):
+            print(imfile1, imfile2)
             image1 = load_image(imfile1)
             image2 = load_image(imfile2)
 
@@ -60,7 +65,10 @@ def demo(args):
             image1, image2 = padder.pad(image1, image2)
 
             flow_low, flow_up = model(image1, image2, iters=20, test_mode=True)
-            viz(image1, flow_up)
+            imfile = os.path.join(args.output, "demo-{}.png".format(i))
+            print(imfile, type(imfile))
+            viz(image1, flow_up, imfile)
+            i += 1
 
 
 if __name__ == '__main__':
@@ -68,8 +76,13 @@ if __name__ == '__main__':
     parser.add_argument('--model', help="restore checkpoint")
     parser.add_argument('--path', help="dataset for evaluation")
     parser.add_argument('--small', action='store_true', help='use small model')
-    parser.add_argument('--mixed_precision', action='store_true', help='use mixed precision')
-    parser.add_argument('--alternate_corr', action='store_true', help='use efficent correlation implementation')
+    parser.add_argument('--mixed_precision',
+                        action='store_true',
+                        help='use mixed precision')
+    parser.add_argument('--alternate_corr',
+                        action='store_true',
+                        help='use efficent correlation implementation')
+    parser.add_argument('--output', default='result', help="output flow image")
     args = parser.parse_args()
 
     demo(args)
