fe28c7cb9d58ef44a825fc3b90ae4cbdc43bea6b
tomo <tomo14142356@gmail.com>
2021-08-04 15:11:31 +0900

    Add start flag, script copy

--- scripts/bdd100k/test_job.sh
+++ scripts/bdd100k/test_job.sh
@@ -12,14 +12,18 @@ data_root=$(
     cd "$1" || exit
     pwd
 )
+flow_out="$data_root/flow"
+script_name=$(basename "$0")
+script_dir="$data_root/flow"
+mkdir -p "$script_dir"
 
 pushd "$git_root"
 
 python flow_save_bdd100k.py \
-    --output "result" \
+    --output "$data_root/flow" \
     --root "$data_root" \
     --subset "train" \
-    --debug \
+    --start 0 \
     --datanum 10 \
     --format-save "torch_save" \
     --model "$git_root/models/raft-small.pth" \
@@ -30,6 +34,7 @@ python flow_save_bdd100k.py \
     --time-offprint \
     # --localtime-offprint \
     # --all-offprint \
+    # --debug \
     # --all \
     # --random \
     # --format-save "pickle" \
@@ -44,3 +49,4 @@ END_TIMESTAMP=$(date '+%s')
 E_TIME=$(($END_TIMESTAMP-$START_TIMESTAMP))
 echo $E_TIME
 
+cp "$script_name" "$script_dir"
