4c25469dbecfee007951706344cfbe1e6e0c87e8
tomo <tomo14142356@gmail.com>
2021-11-12 17:59:41 +0900

    Add git info

--- scripts/bdd100k/sub_job_sh/train_val_gen_img_job_sub.sh
+++ scripts/bdd100k/sub_job_sh/train_val_gen_img_job_sub.sh
@@ -5,6 +5,23 @@ cur_dir=$(
 )
 git_root=$(git rev-parse --show-toplevel | head -1)
 
+bdd100k_root=$(
+    cd "$1" || exit
+    pwd
+)
+bdd100k_root_path="$bdd100k_root/bdd100k"
+bdd100k_flow_script_path="$bdd100k_root_path/flow/scripts"
+date_str=$(date '+%Y%m%d_%H%M%S')
+
+pushd "$git_root"
+git_out="$bdd100k_flow_script_path/git_out/$date_str"
+mkdir -p "$git_out"
+git status > "$git_out/git_status.txt"
+git log > "$git_out/git_log.txt"
+git diff HEAD > "$git_out/git_diff.txt"
+git rev-parse HEAD > "$git_out/git_head.txt"
+popd
+
 job_dir="$git_root/jobs/gen_flow"
 
 datanum=350
