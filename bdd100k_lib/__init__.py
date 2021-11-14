19996f29774eb8fd7d0a7fbbc1d192e76957775d
tomo <tomo14142356@gmail.com>
2021-11-14 11:51:48 +0900

    Fix import gitignore pattern

--- bdd100k_lib/__init__.py
+++ bdd100k_lib/__init__.py
@@ -1,5 +1,5 @@
 from .video_datasets import FlowBDDDataset
 from .video_datasets import BDD, BDDVideo
 from .utils import viz, preprocessing_imgs, gen_flow_correspondence
-from .utils import normalize_flow, normalize_grid, grid_sample_flow
+from .utils import normalize_flow, normalize_coord, grid_sample_flow
 from .utils import concat_flow, forward_backward_consistency, final_gen_flow
